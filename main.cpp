7a897b0631084c367484b26abbfe1cd7a4e76aa7
Bruce Hong <32785837+brucejh99@users.noreply.github.com>
2018-02-05 15:35:19 -0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,12 +1,85 @@
 #include <iostream>
-#include <string>
+#include <stdio.h>
+#include <string.h>
+#include <cstdlib>
+#include <limits>
+#include <ctime>
+#include <vector>
 
 using namespace std;
 
+int wordSelect(int listLength){
+    srand(time(NULL));
+    int wordNum = (rand() % listLength);
+
+  return wordNum;
+}
+
 int main()
 {
+    vector<string> wordlist;
+    wordlist = { "practice", "test", "words", "add", "more", "dinosaur" };
+
     // random number
-    // get word from list
-    // make _ _ _ string
-    // while loop
+    cout << "Press enter to start." << endl;
+    cin.ignore(100, '\n');
+
+    int wordNum = wordSelect(wordlist.size());
+    string currWord = wordlist.at(wordNum);
+    int wordLength = currWord.length();
+    char wordArray[wordLength + 1];
+    strcpy(wordArray, currWord.c_str());
+
+    int lettersGuessed = 0;
+    int wrongGuesses = 0;
+
+    char wordSoFar[wordLength];
+    for(int i = 0; i < wordLength; i++) {
+      wordSoFar[i] = 95;
+    }
+
+    cout << "The initial state is:" << endl;
+    for(int i = 0; i < wordLength; i++) {
+      cout << wordSoFar[i] << " ";
+    }
+    cout << '\n';
+
+    // gameplay (guessing letters and printing)
+    while((lettersGuessed < wordLength) && (wrongGuesses < 6)) {
+      cout << "You have " << 6 - wrongGuesses << " wrong guesses left." << endl;
+
+      cout << "Guess a letter: ";
+      int rightThisRound = 0;
+      char letterGuess;
+      cin >> letterGuess;
+
+      for(int i = 0; i < wordLength; i++) {
+
+        if((wordArray[i] == letterGuess) || (wordArray[i] == letterGuess + 32) ||
+          (wordArray[i] == letterGuess - 32)) {
+            wordSoFar[i] = wordArray[i];
+            cout << wordSoFar[i] << " ";
+            lettersGuessed++;
+            rightThisRound++;
+          }
+        else {
+            cout << wordSoFar[i] << " ";
+        }
+      }
+      cout << endl;
+      if(rightThisRound == 0) {
+        wrongGuesses++;
+      }
+      cout << endl;
+    }
+
+    if(lettersGuessed == wordLength) {
+      cout << "Congrats, you win!" << endl;
+    }
+    else {
+      cout << "Sorry, you lose! The word was " << currWord << "." << endl;
+    }
+    return 0;
 }
+
+
